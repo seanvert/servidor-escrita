12d424e05f91c370f83c1b0f1d7e581ddb3300c4
Sean Uchida <seankuchida@gmail.com>
2022-11-24 09:57:51 -0300

    activity stack added to user schema

--- models/users.js
+++ models/users.js
@@ -3,40 +3,51 @@ const { Schema } = mongoose;
 const passportLocalMongoose = require("passport-local-mongoose");
 const Text = require("../models/texts");
 
+const semester = 7 * 4 * 6;
+
 const User = new Schema({
-  name: String,
-  last_name: String,
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  account_creation: { type: Date, default: Date.now },
-  last_login: { type: Date, default: Date.now },
-  texts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Text",
-    },
-  ],
-  configs: [
-    {
-      exercise: {
-        type: Schema.Types.ObjectId,
-        ref: "Exercise",
-      },
-      config: {
-        time: Number,
-        default: Boolean,
-      },
-    },
-  ],
-  activity: [],
+	name: String,
+	last_name: String,
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+	},
+	account_creation: { type: Date, default: Date.now },
+	last_login: { type: Date, default: Date.now },
+	texts: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Text",
+		},
+	],
+	configs: [
+		{
+			exercise: {
+				type: Schema.Types.ObjectId,
+				ref: "Exercise",
+			},
+			config: {
+				time: Number,
+				default: Boolean,
+			},
+		},
+	],
+	// stack tracking activity
+	activity: {
+		index: Number,
+		dateLastActive: { type: Date, default: Date.now },
+		activityArray: {
+			type: Array,
+			default: new Array(semester).fill(0),
+		},
+	},
 });
 
-// TODO: colocar um vetor de inteiros no activity
-// renderizar ele "de trás pra frente"
+// TODO: check how to make methods for models
+// TODO: add those activity stack operations defined in the text route 
+
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", User);
