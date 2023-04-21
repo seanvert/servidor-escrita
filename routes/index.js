8e1619ea8e85b0895d3aec9ead207ab3b92a176d
Sean Uchida <seankuchida@gmail.com>
2023-04-21 16:10:13 -0300

    middleware added to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const isLoggedIn = require('../middleware/isLoggedIn');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	console.log(req);
+router.get('/', isLoggedIn, function(req, res, next) {
+	// console.log(req);
+	// TODO: healthcheck da api
 	res.send('index');
 });
 
