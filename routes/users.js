ee1a690c60c4bbd9389228b6ba3a35b1becbaf9e
Sean Uchida <seankuchida@gmail.com>
2023-04-21 16:10:58 -0300

    serialialize and disabled unused routes

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,38 @@
+const isLoggedIn = require('../middleware/isLoggedIn');
 var express = require('express');
 var router = express.Router();
 const User = require('../models/users');
+var passport = require("passport");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	console.log(req.body);
-	res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+// 	console.log(req.body);
+// 	res.send('respond with a resource');
+// });
 
-router.get('/new', (req, res, next) => {
-	res.send('rota new formulário criar');
-});
+// router.get('/new', (req, res, next) => {
+// 	res.send('rota new formulário criar');
+// });
 
-router.post('/', (req, res, next) => {
-	console.log(req.body);
-	// TODO: checa se o usuário existe
-	// TODO: redirecionar para outra página
-	res.send("rota post novo usuário");
-});
+// router.post('/', (req, res, next) => {
+// 	console.log(req.body);
+// 	// TODO: checa se o usuário existe
+// 	// TODO: redirecionar para outra página
+// 	res.send("rota post novo usuário");
+// });
 
-router.get('/:id', (req, res, next) => {
-	res.send('rota get id mostra perfil');
-});
+// router.get('/:id', (req, res, next) => {
+// 	res.send('rota get id mostra perfil');
+// });
 
-router.get('/:id/edit', (req, res, next) => {
-	res.send('rota edit formulário de editar perfil');
-});
+// router.get('/:id/edit', (req, res, next) => {
+// 	res.send('rota edit formulário de editar perfil');
+// });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', isLoggedIn, (req, res, next) => {
 	const updatedUser = req.body.user;
 	const filter = {
 		_id: req.user._id
@@ -36,8 +41,8 @@ router.put('/:id', (req, res, next) => {
 	res.json(newUser);
 });
 
-router.delete('/:id', (req, res, next) => {
-	res.send('rota delete dos usuários');
-});
+// router.delete('/:id', (req, res, next) => {
+// 	res.send('rota delete dos usuários');
+// });
 
 module.exports = router;
