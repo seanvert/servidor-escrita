466e860aaae186038c56431a6d29b684555ed4bb
Sean Uchida <seankuchida@gmail.com>
2023-04-21 15:41:21 -0300

    stub exercises templates

---
+++ consts/exercisesDescriptions.js
@@ -0,0 +1,49 @@
+// exercises templates
+// TODO ver se esse negócio vai ficar o padrão mesmo,
+// ou se seria uma boa ideia ter vários conjuntos e exercícios padrão
+// com "linhas" diferentes ou o que seja
+var exercisesTemplates = [
+	{
+		name: 'Escrita Automática',
+		contents: "",
+		type: 0,
+		defaultConfigs: {
+			time: 120,
+			defaultExercise: true
+		}
+	},
+	{
+		name: 'Associação Livre',
+		contents: "",
+		type: 1,
+		defaultConfigs: {
+			time: 120,
+		}
+	},
+	{
+		name: 'Fluxo Verbal',
+		contents: "",
+		type: 2,
+		defaultConfigs: {
+			time: 120,
+		}
+	},	
+	{
+		name: 'Criação de textos a partir de palavras geradoras',
+		contents: "",
+		type: 3,
+		defaultConfigs: {
+			time: 120,
+		}
+	},
+	{
+		name: 'Criação de textos a partir de suas palavras',
+		contents: "",
+		type: 4,
+		defaultConfigs: {
+			time: 120,
+		}
+	}
+]
+
+module.exports = exercisesTemplates;
