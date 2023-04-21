005ec24018097aee4db7603b304c828643de13ee
Sean Uchida <seankuchida@gmail.com>
2023-04-21 16:03:47 -0300

    fix formatting

--- models/texts.js
+++ models/texts.js
@@ -4,8 +4,14 @@ const { Schema } = mongoose;
 const textSchema = new Schema({
 	name:  String, // String is shorthand for {type: String}
 	contents: String,
-	owner: Schema.Types.ObjectId,
-	creation: { type: Date, default: Date.now },
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+	creation: {
+		type: Date,
+		default: Date.now
+	},
 });
 
 module.exports = mongoose.model('Text', textSchema);
