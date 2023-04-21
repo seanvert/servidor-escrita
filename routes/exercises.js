b73ad3014f36036d337cfda686a6b9fb2356b0ab
Sean Uchida <seankuchida@gmail.com>
2023-04-21 16:08:09 -0300

    auth middleware added to routes

--- routes/exercises.js
+++ routes/exercises.js
@@ -7,6 +7,7 @@ var passport = require("passport");
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+router.get('/', isLoggedIn, function(req, res, next) {
 	const filter = {};
 	var json = {};
 	Exercise.find(filter, function(err, exercises) {
@@ -20,7 +21,7 @@ router.get('/new', (req, res, next) => {
 	res.send('formulário para criar novo exercício');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', isLoggedIn, (req, res, next) => {
 	Exercise.create({
 		name: req.body.name,
 		contents: req.body.contents,
@@ -40,20 +41,28 @@ router.post('/', (req, res, next) => {
 
 });
 
-router.get('/:id', (req, res, next) => {
-	res.send('rota get exercício');
+router.get('/:id', isLoggedIn, (req, res, next) => {
+	var exerciseDescription = {};
+	Exercise.findById(req.params.id, function(err, exercise) {
+		exerciseDescription.response = exerciseMaker(exercise);
+		console.log(exerciseDescription);
+		res.json(exerciseDescription);
 });
 
 router.get('/:id/edit', (req, res, next) => {
 	res.send('rota formulário para editar exercício');
 });
 
-router.put('/:id', (req, res, next) => {
-	res.send('rota atualizar exercício');
-});
+// router.get('/:id/edit', (req, res, next) => {
+// 	res.send('rota formulário para editar exercício');
+// });
 
-router.delete('/:id', (req, res, next) => {
-	res.send('rota apagar exercício');
-});
+// router.put('/:id', (req, res, next) => {
+// 	res.send('rota atualizar exercício');
+// });
+
+// router.delete('/:id', (req, res, next) => {
+// 	res.send('rota apagar exercício');
+// });
 
 module.exports = router;
