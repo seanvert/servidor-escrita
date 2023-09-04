857d112305f19902be761466e9f1fa4480ca673f
Sean Uchida <seankuchida@gmail.com>
2023-09-04 11:54:55 -0300

    adicionei uma descrição tirada do livro

--- services/exerciseMaker.js
+++ services/exerciseMaker.js
@@ -27,7 +27,21 @@ function exerciseMaker (exercise) {
 	switch (exercise.name) {
 	case 'Escrita Automática':
 		return {
-			description: ""
+			description: `A escrita automática é uma das mais fortes experiências de liberação,
+			 mexendo praticamente com todas as estruturas da nossa linguagem. Muitos bloqueios 
+			 que impedem a nossa escrita livre e criadora, o nosso escrever solto e espontâneo,
+			  são quebrados de cada vez. Este exercício, que também foi um dos métodos da psicanálise
+			   para a investigação do inconsciente, é muito simples, exatamente como Breton o caracteriza:
+			    pegar uma folha de papel e ir escrevendo livremente, de um modo solto e despreocupado, para
+				 tudo que vier à cabeça sem parar de ler, sem parar para raciocinar, sem analisar, escrever 
+				 livremente, do modo mais solto possível, deixando o pensamento e a linguagem e as mãos e o 
+				 corpo completamente à vontade... Assim, a mão deve ir escrevendo cada vez mais rapidamente, 
+				 mais rapidamente, até acompanhar o ritmo do pensamento, sem nenhuma censura – o que vier à 
+				 cabeça irá para o papel. Se parecer algum bloqueio, alguma censura, deve ir também para o 
+				 papel (por exemplo: “e agora?” “estou bloqueado!”, “o que faço?”, “acabou a inspiração!”, 
+					“quero parar!” etc.). Leve a experiência até o fim (se for preciso, enfrente o medo). 
+					Depois faça o que você quiser: ler, rasgar, guardar. Como você se sentir melhor. 
+					O importante para nós é o processo da escrita sem censura de nenhum tipo.`
 		}
 	case 'Associação Livre':
 		return {
@@ -51,7 +65,7 @@ function exerciseMaker (exercise) {
 		}
 	default:
 		return({
-			description: "vou colocar alguma coisa auqi",
+			description: "vou colocar alguma coisa aqui" + exercise.name,
 		});
 
 	}
