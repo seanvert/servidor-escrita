25239a761ac0d8c7231da6fec2c9844acb65b78b
Sean Uchida <seankuchida@gmail.com>
2023-10-18 14:20:42 -0300

    api healthcheck not tested

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const isLoggedIn = require('../middleware/isLoggedIn');
+const mongoose = require('mongoose');
 
-
+const serverStatus = () => {
+	return {
+		state: 'up',
+		dbState: mongoose.STATES[mongoose.connection.readyState]
+	};
+};
 /* GET home page. */
 router.get('/', isLoggedIn, function(req, res, next) {
 	// TODO: healthcheck da api
 	// ver umas coisas para mandar num json
-	res.send('index');
+
+	res.json(serverStatus());
 });
 
 router.post('/', function(req, res, next) {
