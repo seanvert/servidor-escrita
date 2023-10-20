887af6c3c9a22379b7d8887fa5c2890f77941128
Sean Uchida <seankuchida@gmail.com>
2023-10-19 22:54:21 -0300

    configuration constants moved to .env

--- app.js
+++ app.js
@@ -7,71 +7,71 @@ const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const populateDb = require("./populateDb");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var textsRouter = require('./routes/texts');
-var authRouter = require('./routes/auth');
-var exercisesRouter = require('./routes/exercises');
-var quotesRouter = require('./routes/quotes');
-var morgan = require('morgan');
+const morgan = require('morgan');
+const populateDb = require('./populateDb');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const textsRouter = require('./routes/texts');
+const authRouter = require('./routes/auth');
+const exercisesRouter = require('./routes/exercises');
+const quotesRouter = require('./routes/quotes');
+require('dotenv').config();
 
+const { DB_USER } = process.env;
+const { DB_PASS } = process.env;
+const { DB_URL } = process.env;
+const { SECRET_KEY } = process.env;
+const { DEBUG } = process.env;
 const options = {
-	user: "root",
-	pass: "example",
-	useNewUrlParser: true,
-	autoIndex: false, // Don't build indexes
-	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-	family: 4 // Use IPv4, skip trying IPv6
+  user: DB_USER,
+  pass: DB_PASS,
+  useNewUrlParser: true,
+  autoIndex: false, // Don't build indexes
+  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
 };
 
-const DB_URL = "mongodb://localhost:27017";
-
-
 async function dbConnect() {
-	await mongoose.connect(DB_URL, options);
-	mongoose.connection.useDb("escrita");
+  mongoose.connect(DB_URL, options);
+  mongoose.connection.useDb('escrita');
 }
 dbConnect();
 
-//if (true) {
-//	populateDb();
-//}
+if (DEBUG) {
+  populateDb();
+}
 
 // route do healthcheck da api
-const serverStatus = () => {
-	return {
-		state: 'up',
-		dbState: mongoose.STATES[mongoose.connection.readyState]
-	};
-};
+const serverStatus = () => ({
+  state: 'up',
+  dbState: mongoose.STATES[mongoose.connection.readyState],
+});
 
 const app = express();
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
 app.use(cors({
-    origin: ["http://192.168.0.100:3000", "http://192.168.0.101:3000"],
-	allowedHeaders: ['Content-Type', 'Authorization',
-					 'access-control-allow-credentials',
-					'upgrade-insecure-requests'],
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	preflightContinue: "false",
-	credentials: true,
+  origin: ['http://192.168.0.100:3000', 'http://192.168.0.101:3000'],
+  allowedHeaders: ['Content-Type', 'Authorization',
+    'access-control-allow-credentials',
+    'upgrade-insecure-requests'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: 'false',
+  credentials: true,
 }));
 
-app.use(cookieParser('mysecret that I will change later'));
+app.use(cookieParser(SECRET_KEY));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-	// TODO: change secret key
-	// TODO: insert an environment variable here
-	secret: 'mysecret that I will change later',
-	resave: false,
-	saveUninitialized: false,
-	// TODO: insert sessions into database
+  // TODO: change secret key
+  // TODO: insert an environment variable here
+  secret: SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+  // TODO: insert sessions into database
 }));
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -83,4 +83,3 @@ app.use('/texts', textsRouter);
 app.use('/texts', textsRouter);
 app.use('/quotes', quotesRouter);
 module.exports = app;
-
